f6cec792acfde97f6d57bf2b096edf9f4578fae1
Abhinav Pobbati <apobbati@codeblock.io>
2018-02-11 18:21:27 -0500

    fix: Render mobile headers

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,8 +3,10 @@ import {
   Segment
 } from 'semantic-ui-react'
 
-export default ({children, ...rest}) => (
-  <Segment style={{ padding: '4em 0em', minHeight: '90vh' }} vertical {...rest}>
-    {children}
-  </Segment>
-)
+export default ({children, ...rest}) => {
+  return (
+    <Segment style={{ padding: '4em 0em', minHeight: '90vh' }} vertical {...rest}>
+      {children}
+    </Segment>
+  )
+}
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -10,33 +10,64 @@ import {
 } from 'semantic-ui-react'
 import Layout from '../components/Layout'
 
-export default withSiteData(() => {
-  const { mobile } = {...Responsive.onlyMobile}
+const styles = {
+  h1: {
+    fontSize: '4em',
+    fontWeight: 'normal',
+    marginBottom: 0,
+    marginTop: '3em',
+  },
+  h2: {
+    fontSize: '1.7em',
+    fontWeight: 'normal',
+    marginTop: '1.5em',
+    marginBottom: '1.5em'
+  }
+}
 
+const mobileStyles = {
+  h1: {
+    ...styles.h1,
+    fontSize: '2em',
+    marginTop: '3em',
+  },
+  h2: {
+    ...styles.h2,
+    fontSize: '1.5em',
+    marginTop: '0.5em',
+    marginBottom: '0.5em'
+  }
+}
+
+const Content = ({ mobile = false }) => (
+  <div>
+    <Header
+      as='h1'
+      content='Everything-as-Code'
+      inverted
+      style={mobile ? mobileStyles.h1 : styles.h1}
+    />
+    <Header
+      as='h2'
+      content='Turbocharge your DevSecOps'
+      inverted
+      style={mobile ? mobileStyles.h2 : styles.h2}
+    />
+  </div>
+)
+
+export default withSiteData(() => {
   return (
     <Layout inverted textAlign='center'>
       <Container text>
-        <Header
-          as='h1'
-          content='Everything-as-Code'
-          inverted
-          style={{
-            fontSize: mobile ? '2em' : '4em',
-            fontWeight: 'normal',
-            marginBottom: 0,
-            marginTop: mobile ? '1.5em' : '3em',
-          }}
-        />
-        <Header
-          as='h2'
-          content='Turbocharge your DevSecOps'
-          inverted
-          style={{
-            fontSize: mobile ? '1.5em' : '1.7em',
-            fontWeight: 'normal',
-            marginTop: mobile ? '0.5em' : '1.5em',
-          }}
-        />
+        <Responsive {...Responsive.onlyMobile}>
+          <Content mobile/>
+        </Responsive>
+
+        <Responsive {...Responsive.onlyComputer}>
+          <Content />
+        </Responsive>
+
         <Button primary size='huge'>
           Get Started
           <Icon name='right arrow' />
