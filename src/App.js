fdda99c529509fc1e7e63f7ab8f609b484694fed
Abhinav Pobbati <apobbati@codeblock.io>
2018-02-11 18:58:40 -0500

    fix: Sidebar not opening

--- src/App.js
+++ src/App.js
@@ -94,7 +94,7 @@ class MobileContainer extends Component {
             <Menu.Item as={NavLink} to='/about' exact>About</Menu.Item>
           </Sidebar>
 
-          <Sidebar.Pusher dimmed={sidebarOpened} style={{ minHeight: '100vh' }} onClick={this.handleClose}>
+          <Sidebar.Pusher dimmed={sidebarOpened} style={{ minHeight: '100vh' }}>
             <Segment inverted textAlign='center' style={{ padding: '1em 0em' }} vertical>
               <Container>
                 <Menu inverted pointing secondary size='large'>
