428a197459cd18de0e94b9c32f7f9f8a277aee09
Abhinav Pobbati <apobbati@codeblock.io>
2018-02-12 19:22:27 -0500

    fix: Remove slaask

--- static.config.js
+++ static.config.js
@@ -1,6 +1,5 @@
 import axios from 'axios'
 import React, { Component } from 'react'
-import { ServerStyleSheet } from 'styled-components'
 
 export default {
   preact: true,
@@ -39,30 +38,5 @@ export default {
         component: 'src/containers/404',
       },
     ]
-  },
-  renderToHtml: (render, Comp, meta) => {
-    const sheet = new ServerStyleSheet()
-    const html = render(sheet.collectStyles(<Comp />))
-    meta.styleTags = sheet.getStyleElement()
-    return html
-  },
-  Document: class CustomHtml extends Component {
-    render () {
-      const { Html, Head, Body, children, renderMeta } = this.props
-
-      return (
-        <Html>
-          <Head>
-            <meta charSet="UTF-8" />
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
-            {renderMeta.styleTags}
-          </Head>
-          <Body>
-            {children}
-            <div dangerouslySetInnerHTML={{__html: '<script src="https://cdn.slaask.com/chat.js"></script><script>_slaask.init("e27cbd6eb12e9064a4f4b69a156588bc");</script>'}} />
-          </Body>
-        </Html>
-      )
-    }
   },
 }
